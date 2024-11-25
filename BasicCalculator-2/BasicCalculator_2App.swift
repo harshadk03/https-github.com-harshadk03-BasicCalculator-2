3b05bade14095b7b67d3bf195f4645bad9d088dc
Harshad Kulkarni <contact@harshadkulkarni.com>
2024-11-25 09:21:41 -0800

    Initial Commit

---
+++ BasicCalculator-2/BasicCalculator_2App.swift
@@ -0,0 +1,17 @@
+//
+//  BasicCalculator_2App.swift
+//  BasicCalculator-2
+//
+//  Created by Harshad Kulkarni on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicCalculator_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicCalculator-2Tests/BasicCalculator_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  BasicCalculator_2Tests.swift
+//  BasicCalculator-2Tests
+//
+//  Created by Harshad Kulkarni on 11/25/24.
+//
+
+import Testing
+@testable import BasicCalculator_2
+
+struct BasicCalculator_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
